2f222bf5af8b8785bb5ccfcc0b183a949d7d7d34
lswatson16 <lauralynw16@gmail.com>
2022-05-15 06:31:40 -0400

    Create and import stylesheets for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import './Header.css'
 
 const Header = () => {
   return(
-    <header>
-      Fun With AI
-    </header>
+    <div className="header">
+      <header>
+        Fun With AI
+      </header>
+    </div>
   )
 }
 
