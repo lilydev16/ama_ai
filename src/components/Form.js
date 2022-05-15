087e4f821da5b21f7cf9eb406288b4ede689977f
lswatson16 <lauralynw16@gmail.com>
2022-05-15 03:13:11 -0400

    Create a form and method that updates the Form's state when a user types in the input

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,31 @@ import { Component } from 'react';
 class Form extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      promptInput: ''
+    }
+  }
+
+  updatePromptInput = (e) => {
+    this.setState({ promptInput: e.target.value })
   }
 
   render = () => {
     return(
-      <div>Form</div>
+      <div className='form'>
+        <form>
+          <label htmlFor='prompt'>Prompt:</label>
+          <input 
+            type='text'
+            id='prompt'
+            name='prompt'
+            placeholder='Prompt'
+            value={this.state.promptInput}
+            onChange={(e) => this.updatePromptInput(e)}
+          />
+          <button>Enter</button>
+        </form>
+      </div>
     )
   }
 }
