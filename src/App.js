ec30286642aee44c08e04cb073f9efb5ff4e6ae9
lswatson16 <lauralynw16@gmail.com>
2022-05-15 05:47:53 -0400

    Create and import a header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Component } from 'react';
 import Form from './components/Form/Form';
 import Grid from './components/Grid/Grid';
+import Header from './components/Header/Header';
 
 class App extends Component {
   constructor() {
@@ -18,6 +19,7 @@ class App extends Component {
   render = () => {
     return (
       <div className="app">
+        <Header />
         <Form addPrompt={this.addPrompt} />
         <Grid prompts={this.state.prompts} />
       </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return(
+    <header>
+      Fun With AI
+    </header>
+  )
+}
+
+export default Header;
