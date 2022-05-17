3224be04135180f849fe0a911bccfcb5ede5761c
lswatson16 <lauralynw16@gmail.com>
2022-05-17 09:10:57 -0400

    Make a successful POST to the api that returns the text prompt and response from the AI

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,34 @@
 const apiCalls = {
   postPrompt(newPrompt) {
-    console.log(newPrompt)
-    
+    const url = 'https://api.openai.com/v1/engines/text-curie-001/completions'
+    const apiKey = process.env.REACT_APP_OPENAI_API_KEY
     const data = {
       prompt: newPrompt,
       temperature: 0.5,
-      max_tokens: 64,
+      max_tokens: 6,
       top_p: 1.0,
       frequency_penalty: 0.0,
-      presence_penalty: 0.0
+      presence_penalty: 0.0,
+      echo: true, 
+      stream: false,
+      // n: 1
     }
 
-    console.log(data)
+    console.log(JSON.stringify(data))
 
-    // return fetch()
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`
+      },
+      body: JSON.stringify(data)       
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.choices[0].text)
+      return data.choices[0].text
+    })
   }
 }
 
