01efe111ea7dcf918aec3166fa74c503d9842373
lswatson16 <lauralynw16@gmail.com>
2022-05-18 15:15:05 -0400

    Convert App from a class component to a functional component with react hooks

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
 import './App.css';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Form from './components/Form/Form';
 import List from './components/List/List';
 import Header from './components/Header/Header';
 import apiCalls from './apiCalls';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      texts: [],
-      error: ''
-    }
-  }
+const App = () => {
+  const [texts, setTexts] = useState([])
+  const [error, setError] = useState('')
 
-  addPrompt = (newPrompt) => {
+  const addPrompt = (newPrompt) => {
     apiCalls.postPrompt(newPrompt)
       .then(textData => {
-        this.setState({ texts: [textData, ...this.state.texts]})
+        setTexts([textData, ...texts])
       })
       .catch(err => {
-        this.setState({ error: `Oops... Something went wrong. Our team is working on fixing the issue` })
+        setError(`Oops... Something went wrong. Our team is working on fixing the issue`)
       })
   }
 
-  render = () => {
-    return (
-      <div className="app">
-        {this.state.error ? <p className='error'>{this.state.error}</p> :
-        <>
-          <Header />
-          <Form addPrompt={this.addPrompt} />
-          <List texts={this.state.texts} />
-        </>
-        }
-      </div>
-    )
-  }
+  return (
+    <div className="app">
+      {error ? <p className='error'>{error}</p> :
+      <>
+        <Header />
+        <Form addPrompt={addPrompt} />
+        <List texts={texts} />
+      </>
+      }
+    </div>
+  )
 }
 
 export default App;
