428af7d716fe20d7d9c3d88d0a1e5594bae5d6eb
lswatson16 <lauralynw16@gmail.com>
2022-05-18 23:36:25 -0400

    Add tailwind styling to the inputs and reorganize the structure of the form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -57,54 +57,83 @@ const Form = ({ addPrompt }) => {
   return(
     <div className='form'>
       <form onSubmit={submitPrompt}>
-        <label htmlFor='prompt'>Prompt:</label>
-        <input
-          className='prompt-input'
-          type='text'
-          id='prompt'
-          name='prompt'
-          placeholder='Prompt'
-          value={promptReq.prompt}
-          onChange={(e) => updatePromptInput(e)}
-        />
+        <div>
+          <label htmlFor='prompt' className="block text-sm font-medium text-gray-700">
+            Prompt
+          </label>    
+          <div className="mt-1">
+            <input
+              className='prompt-input shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'
+              aria-describedby="prompt"
+              type='text'
+              id='prompt'
+              name='prompt'
+              placeholder='How are you?'
+              value={promptReq.prompt}
+              onChange={(e) => updatePromptInput(e)}
+              />
+          </div>
+          <p className="mt-2 text-sm text-gray-500" id="prompt">
+            Ask a question.
+          </p>
+        </div>
 
-        <select
-          name='engine'
-          value={promptReq.engine}
-          onChange={(e) => updateEngine(e)}
-        >
-          <option value=''>Choose an engine</option>
-          <option value='text-curie-001'>curie</option>
-          <option value='text-davinci-002'>davinci</option>
-          <option value='text-babbage-001'>babbage</option>
-          <option value='text-ada-001'>ada</option>
-        </select>
-        <p>Choose a temperature</p>
-        <input
-          className='temperature-input'
-          type='number'
-          max='1'
-          min='0'
-          step="0.01"
-          name='temperature'
-          placeholder='0.4'
-          value={promptReq.temperature}
-          onChange={(e) => updateTemperature(e)}
-        />
-        <p>Choose max tokens</p>
-        <input
-          className='tokens-input'
-          type='number'
-          max='140'
-          min='1'
-          name='tokens'
-          placeholder='64'
-          value={promptReq.tokens}
-          onChange={(e) => updateTokens(e)}
-        />
-        <button>Enter</button>
+
+        <div className='settings'>
+          <div>
+            <select
+            className='engine'
+              name='engine'
+              value={promptReq.engine}
+              onChange={(e) => updateEngine(e)}
+            >
+              <option value=''>Choose an engine</option>
+              <option value='text-curie-001'>curie</option>
+              <option value='text-davinci-002'>davinci</option>
+              <option value='text-babbage-001'>babbage</option>
+              <option value='text-ada-001'>ada</option>
+            </select>
+          </div>
+        
+          <div className="flex justify-between">
+            <p className="block text-sm font-medium text-gray-700">Choose a temperature</p>
+            <span className="text-sm text-gray-500" id="temperature">Optional</span>
+          </div>
+          <div className="mt-1">
+            <input
+              className='temperature shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'
+              type='number'
+              max='1'
+              min='0'
+              step="0.01"
+              name='temperature'
+              placeholder='0.4'
+              value={promptReq.temperature}
+              onChange={(e) => updateTemperature(e)}
+              />
+          </div>
+          <div className="flex justify-between">
+            <p className="block text-sm font-medium text-gray-700">Choose max tokens</p>
+            <span className="text-sm text-gray-500" id="tokens">Optional</span>
+          </div>
+          <div className="mt-1">
+            <input
+              className='tokens shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'
+              type='number'
+              max='140'
+              min='1'
+              name='tokens'
+              placeholder='64'
+              value={promptReq.tokens}
+              onChange={(e) => updateTokens(e)}
+              />
+          </div>
+        </div>
+        <div className='button-container'>
+          <button>Enter</button>
+          <button onClick={() => clearInputs()}>Reset</button>
+        </div>
       </form>
-      <button onClick={() => clearInputs()}>Reset</button>
     </div>
   )
 }
