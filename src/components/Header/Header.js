7f391b01b53de7ae267b83ee64366fdb2a1b459a
lswatson16 <lauralynw16@gmail.com>
2022-06-22 11:00:27 -0400

    Remove empty space; Revise to className and single quotes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,11 @@ import React from "react";
 export const Header = () => {
   return(
     <div>
-      < header >
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold leading-tight text-gray-900 text-center">AMA Bot</h1>
+      <header>
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+          <h1 className='text-3xl font-bold leading-tight text-gray-900 text-center'>AMA Bot</h1>
         </div>
-      </header >
+      </header>
     </div>
   )
 }
