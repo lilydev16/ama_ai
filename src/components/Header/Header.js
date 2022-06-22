1137554bc93e01a85b904c94cb660a4fd2c049d6
lswatson16 <lauralynw16@gmail.com>
2022-06-22 11:23:46 -0400

    Add margin top

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ export const Header = () => {
   return(
     <div>
       <header>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-7'>
           <h1 className='text-3xl font-bold leading-tight text-gray-900 text-center'>AMA Bot</h1>
         </div>
       </header>
