86e74581bddb34b652615c43d395641798ada912
lswatson16 <lauralynw16@gmail.com>
2022-06-22 11:48:45 -0400

    Add a fixed height to Footer and prevent the flex item from growing in size

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,7 +17,7 @@ const navigation = [
 
 const Footer = () => {
   return(
-    <div className='mt-7 flex'>
+    <div className='pt-7 flex flex-none h-48'>
       <footer className='bg-white w-full'>
         <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8'>
           <div className='flex justify-center space-x-6 md:order-2'>
