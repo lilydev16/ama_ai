03418502dcc3d059bfd15fc0e2a9f4516487386c
lswatson16 <lauralynw16@gmail.com>
2022-06-27 16:25:05 -0400

    Revise title and header name of application

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ export const Header = () => {
     <div>
       <header>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-7'>
-          <h1 className='text-3xl font-bold leading-tight text-gray-900 text-center'>AMA Bot</h1>
+          <h1 className='text-3xl font-bold leading-tight text-gray-900 text-center'>AMA AI</h1>
         </div>
       </header>
     </div>
